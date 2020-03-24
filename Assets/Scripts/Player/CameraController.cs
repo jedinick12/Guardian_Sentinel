42723f34022e8db9e916a8728e6e4a63331a45fa
roroer <47200820+roroer@users.noreply.github.com>
2020-03-24 15:26:04 -0400

    Added Char controller

---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    public float maxDistance = 5f;
+    public float collisionPadding = 0.1f;
+    [Range(0.01f, 1f)]
+    public float returnSpeed = 0.1f;
+
+    private float mouseX, mouseY;
+
+    public Transform target, player;
+
+    private void Start() {
+        // Lock and hide the cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Set mouse rotation to initial camera rotation
+        Vector3 playerRot = player.eulerAngles;
+        mouseX = playerRot.y;
+    }
+
+    private void LateUpdate() {
+        CamControl();
+
+        CollisionCheck();
+    }
+
+    private void CamControl() {
+        // Get mouse input and clamp it
+        mouseX += Input.GetAxis("Mouse X") * rotationSpeed;
+        mouseY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+    }
+
+    private void CollisionCheck() {
+        Vector3 newLocalPos = transform.localPosition;
+
+        // Get direction from target to camera
+        Vector3 direction = transform.position - target.position;
+        direction.Normalize();
+
+        // Check if something is in between the player and the camera
+        RaycastHit hit;
+        if (Physics.Raycast(target.position, direction, out hit, maxDistance)) {
+            if (hit.transform.tag != "Player" && hit.collider.isTrigger == false) {
+                if (hit.distance < Mathf.Abs(transform.localPosition.z)) {
+                    // If camera is behind the hit point, move cam forward
+                    newLocalPos.z = -(hit.distance - collisionPadding);
+                } else {
+                    // If camera is infront of hit point, lerp cam backwards
+                    newLocalPos.z = Mathf.Lerp(newLocalPos.z, -(hit.distance - collisionPadding), returnSpeed);
+                }
+            }
+        } else {
+            // If camera is not at max distance, lerp backwards
+            newLocalPos.z = Mathf.Lerp(newLocalPos.z, -maxDistance, returnSpeed);
+        }
+
+        // Update the camera's local position
+        transform.localPosition = newLocalPos;
+    }
+
+    public float GetMouseX() {
+        return mouseX;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,166 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+        public float speed = 5f;
+        public float gravity = -9f;
+        public float gravFallMultiplier = 1f;
+        public float gravJumpMultiplier = 0.5f;
+        public float jumpHeight = 1f;
+        [Range(0, 1)]
+        public float airControlPercent = 0.8f;
+        public float turnSmoothTime = 0.05f;
+        public float speedSmoothTime = 0.05f;
+        public float animDamping = 0.1f;
+        public bool doubleJumpEnabled;
+
+        private float velocityY;
+        private float turnSmoothVelocity;
+        private float currentSpeed;
+        private float speedSmoothVelocity;
+        private Vector3 additionalRot;
+        private bool canDoubleJump;
+        private float lookForwardTime;
+
+        public CameraController camController;
+        //public Animator animator;
+
+        private CharacterController controller;
+
+        private void Awake() {
+            controller = GetComponent<CharacterController>();
+            // navMeshAgent.updateRotation = false;
+            if (!camController) {
+                Debug.LogError("Player controller requires reference to camera controller.");
+            }
+        }
+
+        private void Update() {
+            // Get movement input
+            Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+            // Align rotation with camera
+            AlignRotationWithCam(input);
+
+            // Reset double jump if grounded
+            if (controller.isGrounded) {
+                canDoubleJump = true;
+
+                // Update animator
+                //animator.SetBool("isGrounded", true);
+            } else {
+                // Update animator
+                //animator.SetBool("isGrounded", false);
+            }
+
+            // Handle jumping
+            if (Input.GetButtonDown("Jump")) {
+                if (controller.isGrounded) {
+                    Jump();
+                } else if (doubleJumpEnabled && canDoubleJump) {
+                    Jump();
+                    canDoubleJump = false;
+                }
+            }
+
+            // Move
+            Move(input);
+
+            // Animate
+            //animator.SetFloat("speedPercent", currentSpeed / speed, animDamping, Time.deltaTime);
+        }
+
+        private void Move(Vector3 input) {
+            // Rotate player
+            Vector3 inputDir = input.normalized;
+            if (inputDir != Vector3.zero) {
+                // Add rotation according to move direction (this additional rotation is smoothed)
+                float targetAdditionalRot;
+                if (lookForwardTime > 0) {
+                    // Player must look forward (e.g. if attacking)
+                    targetAdditionalRot = Mathf.Atan2(0f, 1f) * Mathf.Rad2Deg;
+                    lookForwardTime -= 1f * Time.deltaTime;
+                } else {
+                    // Not forced to look forward, rotate based on input
+                    targetAdditionalRot = Mathf.Atan2(inputDir.x, inputDir.z) * Mathf.Rad2Deg;
+                }
+                additionalRot = Vector3.up * Mathf.SmoothDampAngle(additionalRot.y, targetAdditionalRot, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+                transform.eulerAngles += additionalRot;
+            }
+
+            // Gravity
+            ApplyGravity();
+
+            // Smooth speed
+            float targetSpeed = speed * input.normalized.magnitude;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+            // Move player
+            Vector3 moveDir = Quaternion.AngleAxis(camController.GetMouseX(), Vector3.up) * input.normalized;
+            moveDir.Normalize();
+            Vector3 move = moveDir * currentSpeed;
+            move += Vector3.up * velocityY;
+            controller.Move(move * Time.deltaTime);
+
+            currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+            // If grounded, reset Y velocity
+            if (controller.isGrounded) {
+                velocityY = 0f;
+            }
+        }
+
+        public void LookForward(float time) {
+            lookForwardTime = time;
+        }
+
+        public void SetPosition(Vector3 pos) {
+            transform.position = pos;
+        }
+
+        public void SetYVelocity(float vel) {
+            velocityY = vel;
+        }
+
+        private void Jump() {
+            // Calculate velocity based on jump height
+            float jumpVelocity = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            velocityY = jumpVelocity;
+
+            // Animate
+            //animator.SetTrigger("jump");
+        }
+
+        private void ApplyGravity() {
+            float grav = gravity;
+
+            // Apply different gravity modifiers if player is falling or jumping
+            if (controller.velocity.y < 0) {
+                grav *= gravFallMultiplier;
+            } else if (controller.velocity.y > 0) {
+                grav *= gravJumpMultiplier;
+            }
+
+            velocityY += grav * Time.deltaTime;
+        }
+
+        private float GetModifiedSmoothTime(float smoothTime) {
+            if (controller.isGrounded) {
+                return smoothTime;
+            }
+            if (airControlPercent == 0) {
+                return float.MaxValue;
+            }
+
+            return smoothTime / airControlPercent;
+        }
+
+        private void AlignRotationWithCam(Vector3 move) {
+            // If the player is moving, align the player rotation with the camera (if the reference exists)
+            if (camController && move.magnitude > 0) {
+                transform.rotation = Quaternion.Euler(0, camController.GetMouseX(), 0);
+            }
+        }
+}
