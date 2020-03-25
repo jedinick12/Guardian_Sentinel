3eb26f2615e39bb5ebfb315be553b6ecc77b07d9
PaneraTeddy <47015328+PaneraTeddy@users.noreply.github.com>
2020-03-25 11:03:23 -0400

    Setup enemy folder and main menu
    in Scenes folder, MainMenu created
    Script folder, Enemy folder created

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // TODO: 
+    // setup patrolling point/area(it round shape)
+    // chasing the player in certain distance
+    // enemey HP, and decrease player HP
+    // attack rate, and attack speed or fireball
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
